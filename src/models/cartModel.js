9cd7eb84b8b6322b471a0e4b0b367083d23f0c94
shabnum1 <shabnumhuda7259@gmail.com>
2022-08-08 05:41:27 -0230

    done

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -6,25 +6,25 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const cartSchema = new mongoose.Schema({
     totalPrice: {
         type: Number,
-        require: true,
+        required: true,
         comment: "Holds total price of all the items in the cart"
 
     },
     totalItems: {
         type: Number,
-        require: true,
+        required: true,
         comment: "Holds total number of items in the cart"
     },
 
     items: [
         {
-            productId: { type: mongoose.Schema.ObjectId, ref: 'Product', require: true, unique: true }    // [{pid1,pqnt1},{pid2,pqnt2},{pid3,pqnt3},{pid4,pqnt4},v]
-            , quantity: { type: Number, require: true, min: 1 }, _id: false
+            productId: { type: ObjectId, ref: 'Product', required: true }    // [{pid1,pqnt1},{pid2,pqnt2},{pid3,pqnt3},{pid4,pqnt4},v]
+            , quantity: { type: Number, required: true, min: 1 }, _id: false
         }
     ],
 
     userId: {
-        type: mongoose.Schema.ObjectId,
+        type: ObjectId,
         ref: "User",
         unique: true,
         required: true,
--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,34 +1,34 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 
 
 const orderSchema = new mongoose.Schema({
     totalPrice: {
         type: Number,
-        require: true,
+        required: true,
         comment: "Holds total price of all the items in the order"
 
     },
     totalItems: {
         type: Number,
-        require: true,
+        required: true,
         comment: "Holds total number of items in the order"
     },
     totalQuantity: {
         type: Number,
-        require: true,
+        required: true,
     },
     items: [
         {
-            productId: { type: mongoose.Schema.ObjectId, ref: 'Product', require: true, unique: true }    // [{pid1,pqnt1},{pid2,pqnt2},{pid3,pqnt3},{pid4,pqnt4},v]
+            productId: { type: ObjectId, ref: 'Product', required: true }    // [{pid1,pqnt1},{pid2,pqnt2},{pid3,pqnt3},{pid4,pqnt4},v]
             , quantity: { type: Number, require: true, min: 1 }, _id: false
         }
     ],
 
     userId: {
-        type: mongoose.Schema.ObjectId,
+        type: ObjectId,
         ref: "User",
-        unique: true,
         required: true,
 
     },
